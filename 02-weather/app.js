1dc12b5dc42fbf24234370813cbd56faf63f57c2
Abdulla Mohamed <abulho@caa.internal>
2021-10-27 15:39:06 +0500

    Fetch data from an external API feature

--- 02-weather/app.js
+++ 02-weather/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fetch = require("node-fetch")
+
 const app = express();
 
 //set the view engine and views folder
@@ -12,4 +14,22 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
     console.log("Weather App hearing on http://localhost:3000")
+})
+
+//function to get weather from an external API
+const getWeather = async () => {
+    try {
+        //use Github API for dummy data to see JSON operation
+        const response = await fetch('https://api.github.com/users/github');
+        const data = await response.json();
+        return data;
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+//extract some dummy data
+getWeather().then(data => {
+    console.log(data.name);
 })
