34e384f443f96167eb89824bef5913f61e32c26d
Abdulla Mohamed <abulho@gmail.com>
2021-11-22 14:44:08 +0500

    Refactor to leverage promises from node-fetch

--- 02-weather/app.js
+++ 02-weather/app.js
@@ -8,29 +8,20 @@ const app = express();
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
+// Leverage promises from node-fetch
+let city = 'male';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.API}`;
 app.get("/", (req, res) => {
-    getWeather().then(data => {
-        //put the data in the locals variable so ejs can pick it up
-        app.locals.data = data;
+    fetch(url)
+    .then(raw_data => raw_data.json())
+    .then(json_data => {
+        app.locals.data = json_data;
+        console.log(json_data);
         res.render("index")
     })
+    .catch(err => console.log(err));
 })
 
 app.listen(3000, () => {
     console.log("Weather App hearing on http://localhost:3000")
 })
-
-//function to get weather from an external API
-const getWeather = async () => {
-    try {
-        //use Github API for dummy data to see JSON operation
-        //const response = await fetch('https://api.github.com/users/github');
-        //const response = await fetch('https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=flw&lang=en');
-        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=male&units=metric&appid=${process.env.API}`);
-        const data = await response.json();
-        return data;
-
-    } catch (err) {
-        console.log(err)
-    }
-}
